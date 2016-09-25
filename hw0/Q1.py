5a8bcd6455d50534b662af9f9b2d77306bc47ad1
fo40225 <fo40225@hotmail.com>
2016-09-25 12:11:17 +0800

    hw0 refactoring

--- hw0/Q1.py
+++ hw0/Q1.py
@@ -1,20 +1,20 @@
 import sys
+
 col = int(sys.argv[1])
 fileName = sys.argv[2]
 
-file = open(fileName,"r")
 nums = []
 
+file = open(fileName, "r")
 for line in file:
     cols = line.split()
-    num = float(cols[col])
+    num = cols[col]
     nums.append(num)
-
 file.close()
 
-nums.sort()
-result = ",".join(map(lambda x:"{:.3f}".format(x),nums))
+nums.sort(key=lambda x: float(x))
+result = ",".join(nums)
 
-ans = open("ans1.txt","w")
+ans = open("ans1.txt", "w")
 ans.write(result)
 ans.close()
